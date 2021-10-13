abd8344d6791575273e90ebc35b2e0cd9d29878a
nailanabilaaaaaa <nailanabilaaaaaa@gmail.com>
2021-10-13 09:47:12 +0700

    Update Profil Migration

---
+++ database/migrations/2021_10_10_112031_create_profil_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfilTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profil', function (Blueprint $table) {
+            $table->id();
+            $table->longtext('profil_sekolah');
+            $table->string('motto_sekolah');
+            $table->longtext('visi_sekolah');
+            $table->longtext('misi_sekolah');
+            $table->longtext('tujuan_sekolah');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profil');
+    }
+}
