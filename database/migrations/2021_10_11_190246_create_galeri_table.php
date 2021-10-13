8eba94f1ed0bf0d7d1285b970b315ed2ea6ace72
nailanabilaaaaaa <nailanabilaaaaaa@gmail.com>
2021-10-13 09:51:03 +0700

    Update Galeri Migration

---
+++ database/migrations/2021_10_11_190246_create_galeri_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGaleriTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('galeri', function (Blueprint $table) {
+            $table->id();
+            $table->string('image1');
+            $table->string('content1');
+            $table->string('image2');
+            $table->string('content2');
+            $table->string('image3');
+            $table->string('content3');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('galeri');
+    }
+}
